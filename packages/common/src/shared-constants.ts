292d04e4585c81df7b19eefa7a0fe99fb219a25d
mohamed shaban <mosh7.dev@gmail.com>
2026-06-01 23:12:20 +0300

    chore(common): Initialized common package

---
+++ packages/common/src/shared-constants.ts
@@ -0,0 +1,20 @@
+export const LANGUAGE_CODE_QUERY_NAME = 'languageCode';
+export const CURRENCY_CODE_QUERY_NAME = 'currencyCode';
+export const ADMIN_API_PATH = 'admin-api';
+export const STORE_API_PATH = 'store-api';
+export const DEFAULT_SERVER_PORT = 3000;
+export const DEFAULT_MARKETPLACE_REGION_IDENTIFIER = 'x-marketplace-region';
+export const DEFAULT_MARKETPLACE_REGION_CODE = 'Default Marketplace Region';
+export const DEFAULT_MARKETPLACE_REGION_TOKEN = '__default-marketplace-region__';
+
+export const SUPER_ADMIN_ROLE_CODE = '__super-admin__';
+export const SUPER_ADMIN_ROLE_DESCRIPTION = 'Super Admin';
+
+export const CUSTOMER_ROLE_CODE = '__customer__';
+export const CUSTOMER_ROLE_DESCRIPTION = 'Customer';
+
+export const COMPANY_ADMIN_ROLE_CODE = '__company-admin__';
+export const COMPANY_ADMIN_ROLE_DESCRIPTION = 'Company Admin';
+
+export const DEFAULT_SUPER_ADMIN_IDENTIFIER = 'admin';
+export const DEFAULT_SUPER_ADMIN_PASSWORD = 'admin';
